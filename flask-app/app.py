69d93ed77909e5775be524023dcfe842260f9a11
osbm <osmanfbayram@gmail.com>
2024-01-11 23:55:32 +0300

    update flask

--- flask-app/app.py
+++ flask-app/app.py
@@ -7,20 +7,16 @@ import os
 mongo_uri = os.environ.get('MONGO_URI')
 mongo_client = MongoClient(mongo_uri)
 
-database = mongo_client.test
-collection = database.restaurants
+database = mongo_client.las
 
 app = flask.Flask(__name__)
 
-
-
 @app.route('/')
 def index():
-    data = collection.find_one()
-    app.logger.info(type(data))
-    print(type(data))
-
-    return str(data)
+    all_collections = database.list_collection_names()
+    num_doc_per_collection = { collection: database[collection].count_documents({}) for collection in all_collections }
+    
+    return str(num_doc_per_collection)
 
 
 if __name__ == '__main__':
