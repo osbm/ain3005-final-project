d26e741568d19bb10b861c2a54d9f42e7f0246b7
osbm <osmanfbayram@gmail.com>
2024-01-14 00:28:26 +0300

    add comment lines for later use

--- mongodb-import-data/mongoimport-run.sh
+++ mongodb-import-data/mongoimport-run.sh
@@ -1,2 +1,5 @@
 mongoimport --host mongodb-database --db las --collection books --file books.json --jsonArray --type json
 mongoimport --host mongodb-database --db las --collection users --file users.json --jsonArray --type json
+# mongoimport --host mongodb-database --db las --collection fines --file fines.json --jsonArray --type json
+# mongoimport --host mongodb-database --db las --collection reservations --file reservations.json --jsonArray --type json
+# mongoimport --host mongodb-database --db las --collection loans --file loans.json --jsonArray --type json
